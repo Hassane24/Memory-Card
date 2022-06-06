77e24169fde1e3f50e1de2888cdc3d1b18f3de39
Hassane24 <hsisane5@gmail.com>
2022-06-06 22:41:16 +0100

    randomizing cards working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Score from "./Score/Score";
 import Card from "./Card";
-import killua from "../assets/Killua.jpeg";
 
 const Main = () => {
   const [bestScore, setBestScore] = useState(0);
@@ -20,6 +19,34 @@ const Main = () => {
     { name: "Uvo", id: 10, clicked: false },
     { name: "Shalnark", id: 11, clicked: false },
   ]);
+
+  useEffect(() => {
+    setChars((prevState) => {
+      const newState = [...prevState];
+      for (var i = newState.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newState[i];
+        newState[i] = newState[j];
+        newState[j] = temp;
+      }
+      return newState;
+    });
+  }, [currentScore]);
+
+  const cardClickHandler = () => {
+    setcurrentScore(currentScore + 1);
+    setChars((prevState) => {
+      const newState = [...prevState];
+      for (var i = newState.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newState[i];
+        newState[i] = newState[j];
+        newState[j] = temp;
+      }
+      return newState;
+    });
+  };
+
   return (
     <div className="main">
       <Score bestScore={bestScore} currentScore={currentScore}></Score>
@@ -30,6 +57,7 @@ const Main = () => {
             imgAlt={char.name}
             key={char.id}
             character={char.name}
+            clickEvent={cardClickHandler}
           ></Card>
         ))}
       </div>
